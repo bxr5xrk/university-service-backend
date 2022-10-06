bc49c3b9a8f02c864e72ebaefc0b85c635def22a
palyanytsya <vasyl.dobro.2003@gmail.com>
2022-10-06 21:39:06 +0300

    added subjects field

--- src/lecturer/lecturer.service.ts
+++ src/lecturer/lecturer.service.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from './../prisma/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateLecturerInput, UpdateLecturerInput } from 'src/types/graphql';
+import { addOrRemoveSubject } from 'src/utils';
 
 @Injectable()
 export class LecturerService {
@@ -8,35 +9,43 @@ export class LecturerService {
   create({ fullName }: CreateLecturerInput) {
     return this.prisma.lecturer.create({
       data: { fullName },
-      include: { groups: true },
+      include: { groups: true, subjects: true },
     });
   }
 
   findAll() {
     return this.prisma.lecturer.findMany({
-      include: { groups: true },
+      include: { groups: true, subjects: true },
     });
   }
 
   findOne(id: number) {
     return this.prisma.lecturer.findUnique({
       where: { id },
-      include: { groups: true },
+      include: { groups: true, subjects: true },
     });
   }
 
-  update(id: number, { fullName }: UpdateLecturerInput) {
+  async update(id: number, { fullName, subjectId }: UpdateLecturerInput) {
+    const findSubject = await this.prisma.lecturer.findUnique({
+      where: { id },
+      include: { subjects: true },
+    });
+
     return this.prisma.lecturer.update({
       where: { id },
-      data: { fullName },
-      include: { groups: true },
+      data: {
+        fullName,
+        subjects: addOrRemoveSubject(findSubject.subjects, subjectId),
+      },
+      include: { groups: true, subjects: true },
     });
   }
 
   remove(id: number) {
     return this.prisma.lecturer.delete({
       where: { id },
-      include: { groups: true },
+      include: { groups: true, subjects: true },
     });
   }
 }
